0139e0063749b6d2f7bda11ac0b91eff09b74204
PabloAndresBustos <pabloandresbustos@gmail.com>
2024-07-12 13:35:37 -0300

    Agregamos efecto swiper | add swiper efect

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { register } from 'swiper/element/bundle';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -11,6 +12,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 if (environment.production) {
   enableProdMode();
 }
+
+register();
 
 bootstrapApplication(AppComponent, {
   providers: [
