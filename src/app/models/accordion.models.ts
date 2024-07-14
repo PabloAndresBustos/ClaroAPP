3ff8ab204d6366a1b8f04988c4d496cd6a78c378
PabloAndresBustos <pabloandresbustos@gmail.com>
2024-07-14 18:46:26 -0300

    Agregamos contenido en mas info | add content in more info

--- src/app/models/accordion.models.ts
+++ src/app/models/accordion.models.ts
@@ -2,5 +2,6 @@ export interface Accordion{
     title:string,
     content?:string,
     url?: string,
+    titleList?:string,
     list?: string[]
 }
--- src/app/pages/home/services/services.page.ts
+++ src/app/pages/home/services/services.page.ts
@@ -24,7 +24,8 @@ export class ServicesPage implements OnInit {
 
   pagosOnline:Accordion[] = [
     {title: 'Pagos desde Mi Claro', 
-     content: 'Sin consumir us datos, pagá tus facturas en Mi Claro con tus tarjetas de crédito o con Visa Débito. Pasos a seguir:',
+     content: 'Sin consumir us datos, pagá tus facturas en Mi Claro con tus tarjetas de crédito o con Visa Débito.',
+     titleList: 'Pasos a seguir: ',
      list: [
       'Iniciá sesión en Mi Claro y elegí la opción "Pagar mi Factura".',
       'Conocé el importe a pagar y hace clic en “Continuar”.',
@@ -33,7 +34,8 @@ export class ServicesPage implements OnInit {
      ]
     },
     {title: 'Pagos por Claro Pay', 
-     content: 'Pagá tu factura al instante desde Claro Pay sin consumir tus datos y con beneficios todos los meses. Podes elegir pagar con Dinero en Cuenta o con tus tarjetas de debito o crédito. Entrá ahora en Claro Pay  y empezá a disfrutar tus beneficios.',
+     content: 'Pagá tu factura al instante desde Claro Pay sin consumir tus datos y con beneficios todos los meses. Podes elegir pagar con Dinero en Cuenta o con tus tarjetas de debito o crédito.',
+     titleList: 'Entrá ahora en Claro Pay  y empezá a disfrutar tus beneficios.',
      list: [
       'Ingresá a la opción Pagá tu factura Claro.',
       'Seleccioná el medio de pago con el que quierés pagar.',
@@ -41,8 +43,33 @@ export class ServicesPage implements OnInit {
       'Y listo!'
      ]
     },
-    {title: 'Pagos por PagosMisCuentas.com', content: 'Instructivos'},
-    {title: 'Pagos por Pagos Link', content: 'Instructivos'}
+    {title: 'Pagos por PagosMisCuentas.com', 
+     content: 'Pagá al instante desde www.pagomiscuentas.com o desde tu homebanking.',
+     titleList: 'Pasos a seguir: ',
+     list: [
+      'Ingresar el rubro: "Telefonía".',
+      'Seleccionar la empresa: "Claro".',
+      'Ingresar tu Número de cuenta: son los 11 dígitos que figuran en el frente de la factura.',
+      'Importe: El monto a pagar aparecerá en la plataforma. Si es la primera vez que pagás o a la fecha no tenés emitida una factura, tendrás que ingresarlo. * Podés encontrar tu Número de cuenta en: *',
+      'Tu notificación de Factura.',
+      'Mi Claro.',
+      'O enviando un SMS con la palabra "CUENTA" al 611.'
+     ]
+    },
+    {title: 'Pagos por Pagos Link', 
+     content: 'Pagá desde www.pagar.com.ar  o desde tu homebanking',
+     titleList: 'Pasos a seguir: ',
+     list: [
+      'Ingresar rubro: "Telefonía".',
+      'Seleccionar ente: "Claro".',
+      'Ingresar tu Número de cuenta: son los 11 dígitos que figuran en el frente de la factura.',
+      'Importe: El monto a pagar aparecerá en la plataforma. Si es la primera vez que pagás o a la fecha no tenés emitida una factura, tendrás que ingresarlo. Tu pago se registra dentro de las 48 horas hábiles. Podés agendar estos datos para pagar tu próxima factura. Podés encontrar tu Número de cuenta en:',
+      'Tu notificación de Factura',
+      'Mi Claro',
+      'O enviando un SMS con la palabra "CUENTA" al 611.'
+     ]
+    },
+     
   ]
 
   pagosTelefono: Accordion[] = [
